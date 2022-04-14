e46ff6fddf58783edb9c37bc7376da606f1b9e5d
Martin Rosas <mlrosas21@hotmail.com>
2022-04-13 23:22:27 -0300

    Adding class 20 to project (login)

--- scripts/login.js
+++ scripts/login.js
@@ -8,8 +8,10 @@ window.addEventListener('load', function() {
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
-    form.addEventListener('submit', function(event) {
-
+    const form = document.querySelector('#loginForm');
+    form.addEventListener('submit', function(e) {
+        e.preventDefault
+        console.log(e)
 
 
 
@@ -20,14 +22,38 @@ window.addEventListener('load', function() {
     /* -------------------------------------------------------------------------- */
     /*                     FUNCIÓN 2: Realizar el login [POST]                    */
     /* -------------------------------------------------------------------------- */
-    function realizarLogin(settings) {
+    const urlBase = 'https://ctd-todo-api.herokuapp.com/v1';
+    const keyToken = 'token';
 
+    function realizarLogin(mail, pass) {
+        const urlLogin = urlBase + '/users/login';
+        const settings = {
+            method: 'POST',
+            body: JSON.stringify({ email: mail, password: pass }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
 
-
-
-
-
+        fetch(urlLogin, settings)
+            .then(response => {
+                if (response.status == 400 || response.status == 404) {
+                    showMessage('Credenciales inválidas.');
+                } else if (response.status == 500) {
+                    showMessage('Error al conectarse con el servidor, intente mas tarde.');
+                } else if (response.status == 201) {
+                    return response.json();
+                }
+            })
+            .then(data => {
+                if (data) {
+                    sessionStorage.setItem(keyToken, data.jwt);
+                    location.replace('mis-tareas.html');
+                }
+            });
     };
 
-
+    function showMessage(message) {
+        alert(message);
+    }
 });
